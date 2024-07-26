bdde1eab50df0ba0be44d7884876dce0bc906fc0
Kol.ds <fatbahad@gmail.com>
2024-07-26 21:25:51 +0300

    Add project files.

---
+++ Source/ShootTheFuckUpGame/Public/STFUGameModeBase.h
@@ -0,0 +1,16 @@
+// Shoot The Fuck Up Game. All Rights reserved!!!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "STFUGameModeBase.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SHOOTTHEFUCKUPGAME_API ASTFUGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+};
---
+++ Source/ShootTheFuckUpGame/ShootTheFuckUpGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShootTheFuckUpGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootTheFuckUpGame, "ShootTheFuckUpGame" );
