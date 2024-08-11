7545f1a33cc2c9f81b321a5b61124494f2979a58
Kol.ds <fatbahad@gmail.com>
2024-08-11 10:51:08 +0300

    end player widget work

--- Source/ShootTheFuckUpGame/Private/UI/STFUPlayerHUDWidget.cpp
+++ Source/ShootTheFuckUpGame/Private/UI/STFUPlayerHUDWidget.cpp
@@ -4,39 +4,26 @@
 #include "Components\STFUHealthComponent.h"
 #include "Components\STFUWeaponComponent.h"
 
+
+
 float USTFUPlayerHUDWidget::GetHelthPercent()
 {
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return 0.0f;
-
-    const auto Component       = Player->GetComponentByClass(USTFUHealthComponent::StaticClass());
-    const auto HealthComponent = Cast<USTFUHealthComponent>(Component);
+    const auto HealthComponent = GetPLayerHealthConponent();
     if (!HealthComponent) return 0.0f;
-
     return HealthComponent->GetHealthPercent();
 }
 
 bool USTFUPlayerHUDWidget::GetWeaponUIData(FWeaponUIData& UIData) const
 {
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return false;
-
-    const auto Component       = Player->GetComponentByClass(USTFUWeaponComponent::StaticClass());
-    const auto WeaponComponent = Cast<USTFUWeaponComponent>(Component);
+    const auto WeaponComponent = GetPLayerWeaponConponent();
     if (!WeaponComponent) return false;
-
     return WeaponComponent->GetWeaponUIData(UIData);
 }
 
 bool USTFUPlayerHUDWidget::GetCurrenAmmo(FAmmoData& AmmoData) const
 {
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return false;
-
-    const auto Component       = Player->GetComponentByClass(USTFUWeaponComponent::StaticClass());
-    const auto WeaponComponent = Cast<USTFUWeaponComponent>(Component);
+    const auto WeaponComponent = GetPLayerWeaponConponent();
     if (!WeaponComponent) return false;
-
     return WeaponComponent->GetCurrenAmmo(AmmoData);
 }
 
@@ -52,7 +39,36 @@ FString USTFUPlayerHUDWidget::GetCurrentAmmoText(FAmmoData& AmmoData)
     {
         return FString::Printf(TEXT(" %i / %i"), AmmoData.Bullets, AmmoData.Clips);
     }
-    
 }
 
+bool USTFUPlayerHUDWidget::IsPlayerAlive() const
+{
+    const auto HealthComponent = GetPLayerHealthConponent();
+    return HealthComponent && !HealthComponent->IsDead();
+}
 
+bool USTFUPlayerHUDWidget::IsPlayerSpectating() const
+{
+    const auto Controller = GetOwningPlayer();
+    return Controller && Controller->GetStateName() == NAME_Spectating;
+}
+
+USTFUWeaponComponent* USTFUPlayerHUDWidget::GetPLayerWeaponConponent() const
+{
+    auto Player = GetOwningPlayerPawn();
+    if (!Player) return nullptr;
+
+    auto Component       = Player->GetComponentByClass(USTFUWeaponComponent::StaticClass());
+    auto WeaponComponent = Cast<USTFUWeaponComponent>(Component);
+    return WeaponComponent;
+}
+
+USTFUHealthComponent* USTFUPlayerHUDWidget::GetPLayerHealthConponent() const
+{
+    auto Player = GetOwningPlayerPawn();
+    if (!Player) return nullptr;
+
+    auto Component       = Player->GetComponentByClass(USTFUHealthComponent::StaticClass());
+    auto HealthComponent = Cast<USTFUHealthComponent>(Component);
+    return HealthComponent;
+}
--- Source/ShootTheFuckUpGame/Public/UI/STFUPlayerHUDWidget.h
+++ Source/ShootTheFuckUpGame/Public/UI/STFUPlayerHUDWidget.h
@@ -7,23 +7,34 @@
 #include "Weapon\STFUBaseWeapon.h"
 #include "STFUPlayerHUDWidget.generated.h"
 
+class USTFUHealthComponent;
+class USTFUWeaponComponent;
+
 UCLASS()
 class SHOOTTHEFUCKUPGAME_API USTFUPlayerHUDWidget : public UUserWidget
 {
     GENERATED_BODY()
 
 public:
-
     UFUNCTION(BlueprintCallable, Category = "UI")
     float GetHelthPercent();
 
     UFUNCTION(BlueprintCallable, Category = "UI")
     bool GetWeaponUIData(FWeaponUIData& UIData) const;
 
-
     UFUNCTION(BlueprintCallable, Category = "UI")
     bool GetCurrenAmmo(FAmmoData& AmmoData) const;
 
     UFUNCTION(BlueprintCallable, Category = "UI")
     FString GetCurrentAmmoText(FAmmoData& AmmoData);
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool IsPlayerAlive() const;
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool IsPlayerSpectating() const;
+
+private:
+    USTFUWeaponComponent* GetPLayerWeaponConponent() const;
+    USTFUHealthComponent* GetPLayerHealthConponent() const;
 };
