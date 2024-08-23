5d42d96cb71160aef6af1a6a5cfdbc82af54c4b2
Kol.ds <fatbahad@gmail.com>
2024-08-23 21:47:54 +0300

    add behavior  three

---
+++ Source/ShootTheFuckUpGame/Public/AI/STFUAICharacter.h
@@ -0,0 +1,17 @@
+// Shoot The Fuck Up Game. All Rights reserved!!!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/STFUCharacter.h"
+#include "STFUAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEFUCKUPGAME_API ASTFUAICharacter : public ASTFUCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ShootTheFuckUpGame/Public/AI/STFUAIController.h
@@ -0,0 +1,17 @@
+// Shoot The Fuck Up Game. All Rights reserved!!!
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "STFUAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEFUCKUPGAME_API ASTFUAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
